e1a95edd7e612ef3628537f805c7bfc0ad38eb84
Semapro B.V. <106259735+Semapro@users.noreply.github.com>
2026-07-30 14:52:31 +0200

    Add src/pdf_add_nmbrs/__init__.py

---
+++ src/pdf_add_nmbrs/__init__.py
@@ -0,0 +1,7 @@
+"""Add line numbers to PDF review copies."""
+
+from pdf_add_nmbrs.cli import PdfLineNumberError, add_line_numbers
+
+__all__ = ["PdfLineNumberError", "add_line_numbers"]
+__version__ = "0.1.0"
+
